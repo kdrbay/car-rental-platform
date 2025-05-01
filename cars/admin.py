5df18014317b0fd7a1b6eec0a16f9ef5ee90edde
kdrbay.a <166226803+kdrbay@users.noreply.github.com>
2025-05-01 19:46:42 +0000

    Настроен админский интерфейс для моделей User и Car

--- cars/admin.py
+++ cars/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Car
 
-admin.site.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'year', 'price_per_day', 'location', 'owner')
+    search_fields = ('brand', 'model', 'owner__username')
+    list_filter = ('brand', 'location')
+
+admin.site.register(Car, CarAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'phone_number', 'is_seller', 'is_staff')
+    search_fields = ('username', 'email')
+    list_filter = ('is_seller',)
+
+admin.site.register(User, UserAdmin)
